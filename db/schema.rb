1ebed441d426a7134f7d676bc7bb084e4fda95f9
juroga_new <juanjrgast@gmail.com>
2023-06-18 14:23:23 +0200

    Created Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_110347) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_122242) do
+  create_table "comments", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_110347) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
